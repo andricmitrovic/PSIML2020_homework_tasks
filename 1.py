80492a91f106722858467e10a255bb3ce2ef1aa0
Nikola Andric Mitrovic <andricmitrovicnikola@yahoo.com>
2020-03-14 10:01:21 +0100

    Tuto perfecto

--- 1.py
+++ 1.py
@@ -3,8 +3,8 @@ import re
 
 if __name__ == "__main__":
 
-    #path = input()
-    path = "./wise_man_data/set/1"
+    path = input()
+    #path = "./wise_man_data/set/1"
     #path = "./wise_man"
 
     correct_file = re.compile(r'^ca[0-9]*.txt$')
@@ -12,9 +12,6 @@ if __name__ == "__main__":
 
     P = 0
     N = 0
-
-    TP = 0
-    FP = 0
 
     valid_P = 0
     valid_N = 0
@@ -33,8 +30,6 @@ if __name__ == "__main__":
                         file_indexes[index] = [1, -1]
                     else:
                         file_indexes[index][0] = 1
-                        if file_indexes[index][1] >= 70:
-                            TP += 1
                         valid_P += 1
                 else:
                     N += 1
@@ -42,8 +37,6 @@ if __name__ == "__main__":
                         file_indexes[index] = [0, -1]
                     else:
                         file_indexes[index][0] = 0
-                        if file_indexes[index][1] >= 70:
-                            FP += 1
                         valid_N += 1
                 f.close()
             elif wise_file.search(file):
@@ -57,10 +50,8 @@ if __name__ == "__main__":
                     else:
                         file_indexes[index][1] = threshold
                         if file_indexes[index][0] == 1:
-                            TP += 1
                             valid_P += 1
                         else:
-                            FP += 1
                             valid_N += 1
 
                 else:
@@ -75,12 +66,6 @@ if __name__ == "__main__":
 
                 f.close()
 
-    TPR = TP/valid_P
-    FPR = FP/valid_N
-
-    print(P, N, valid_P+valid_N, round(TPR, 3), round(FPR, 3))
-
-
     TP = 0
     FP = 0
 
@@ -90,6 +75,23 @@ if __name__ == "__main__":
         if answer[0] == 0 and answer[1] >= 70:
             FP += 1
 
+    TPR = TP/valid_P
+    FPR = FP/valid_N
 
-    print(round(TPR, 3), round(FPR, 3))
+    #optimizuj ne mora sve thresholdove da gleda
+    for threshold in range(0, 101):
+        TP = 0
+        FP = 0
+        for key, answer in file_indexes.items():
+            if answer[0] == 1 and answer[1] >= threshold:
+                TP += 1
+            if answer[0] == 0 and answer[1] >= threshold:
+                FP += 1
+        TPR_try = TP/valid_P
+        FPR_try = FP/valid_N
+        #print(round(1-TPR_try, 3), round(FPR_try, 3), abs(round(1 - TPR_try, 3) - round(FPR_try, 3)))
+        if abs(round(1 - TPR_try, 3) - round(FPR_try, 3)) <= 0.01:
+            break
 
+
+    print(f'{P},{N},{valid_P+valid_N},{round(TPR, 3)},{round(FPR, 3)},{round(FPR_try, 3)}')
